9571c1b1ed7883ae298a2d3996b4597ea6e4c48c
Sofa <muhammadsofa52@yahoo.com>
2024-06-11 09:21:06 +0700

    Contact Model

---
+++ app/Models/Contact.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Contact extends Model
+{
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    protected $table = "contacts";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(Contact::class, "user_id", "id");
+    }
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Model
 {
@@ -12,4 +13,8 @@ class User extends Model
     protected $keyType = "int";
     public $timestamps = true;
     public $incrementing = true;
+
+    public function contacts(): HasMany {
+        return $this->hasMany(Contact::class, "user_id", "id");
+    }
 }
